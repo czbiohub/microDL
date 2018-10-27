1cacb29ca2dabcab4e537dbcd8bf08cc0ccf1231
sguo <symguo@gmail.com>
2018-10-26 18:13:18 -0700

    Add required modules for DSSIM loss from keras-contrib

--- micro_dl/train/losses.py
+++ micro_dl/train/losses.py
@@ -1,8 +1,8 @@
 """Custom losses"""
 from keras import backend as K
+import micro_dl.keras_contrib.backend as KC
 import tensorflow as tf
 import micro_dl.train.metrics as metrics
-
 
 class DSSIM_Loss():
     def __init__(self, k1=0.01, k2=0.03, kernel_size=3, max_value=1.0):
@@ -25,11 +25,11 @@ class DSSIM_Loss():
         self.max_value = max_value
         self.c1 = (self.k1 * self.max_value) ** 2
         self.c2 = (self.k2 * self.max_value) ** 2
-        self.dim_ordering = K.image_data_format()
-        self.backend = K.backend()
+        self.dim_ordering = KC.image_data_format()
+        self.backend = KC.backend()
 
     def __int_shape(self, x):
-        return K.int_shape(x) if self.backend == 'tensorflow' else K.shape(x)
+        return KC.int_shape(x) if self.backend == 'tensorflow' else KC.shape(x)
 
     def __call__(self, y_true, y_pred):
         # There are additional parameters for this function
@@ -37,29 +37,32 @@ class DSSIM_Loss():
         #   and cannot be used for learning
 
         kernel = [self.kernel_size, self.kernel_size]
-        y_true = K.reshape(y_true, [-1] + list(self.__int_shape(y_pred)[1:]))
-        y_pred = K.reshape(y_pred, [-1] + list(self.__int_shape(y_pred)[1:]))
+        y_true = KC.reshape(y_true, [-1] + list(self.__int_shape(y_pred)[1:]))
+        y_pred = KC.reshape(y_pred, [-1] + list(self.__int_shape(y_pred)[1:]))
 
         patches_pred = tf.extract_image_patches(y_pred, kernel, kernel, 'valid', self.dim_ordering)
         patches_true = tf.extract_image_patches(y_true, kernel, kernel, 'valid', self.dim_ordering)
 
+        patches_true = tf.extract_image_patches(y_true, [1, 5, 5, 1], [1, 2, 2, 1], [1, 1, 1, 1], "VALID")
+        patches_pred = tf.extract_image_patches(y_pred, [1, 5, 5, 1], [1, 2, 2, 1], [1, 1, 1, 1], "VALID")
+
         # Reshape to get the var in the cells
         bs, w, h, c1, c2, c3 = self.__int_shape(patches_pred)
-        patches_pred = K.reshape(patches_pred, [-1, w, h, c1 * c2 * c3])
-        patches_true = K.reshape(patches_true, [-1, w, h, c1 * c2 * c3])
+        patches_pred = KC.reshape(patches_pred, [-1, w, h, c1 * c2 * c3])
+        patches_true = KC.reshape(patches_true, [-1, w, h, c1 * c2 * c3])
         # Get mean
-        u_true = K.mean(patches_true, axis=-1)
-        u_pred = K.mean(patches_pred, axis=-1)
+        u_true = KC.mean(patches_true, axis=-1)
+        u_pred = KC.mean(patches_pred, axis=-1)
         # Get variance
-        var_true = K.var(patches_true, axis=-1)
-        var_pred = K.var(patches_pred, axis=-1)
+        var_true = KC.var(patches_true, axis=-1)
+        var_pred = KC.var(patches_pred, axis=-1)
         # Get std dev
-        covar_true_pred = K.mean(patches_true * patches_pred, axis=-1) - u_true * u_pred
+        covar_true_pred = KC.mean(patches_true * patches_pred, axis=-1) - u_true * u_pred
 
         ssim = (2 * u_true * u_pred + self.c1) * (2 * covar_true_pred + self.c2)
-        denom = (K.square(u_true) + K.square(u_pred) + self.c1) * (var_pred + var_true + self.c2)
+        denom = (KC.square(u_true) + KC.square(u_pred) + self.c1) * (var_pred + var_true + self.c2)
         ssim /= denom  # no need for clipping, c1 and c2 make the denom non-zero
-        return K.mean((1.0 - ssim) / 2.0)
+        return KC.mean((1.0 - ssim) / 2.0)
 
 
 def dssim_loss(y_true, y_pred):
