43e8c9fb63cda01f8c8e073bdc4dde78752f6c27
sguo <symguo@gmail.com>
2018-10-27 23:19:54 -0700

    add new ssim loss

--- tests/DSSIM_test.py
+++ tests/DSSIM_test.py
@@ -1,13 +1,14 @@
 import numpy as np
 from keras import backend as K
 from micro_dl.train.losses import dssim_loss
+import tensorflow as tf
 
 #Shape should be (batch,x,y,channels)
 imga = np.random.normal(size=(4,256,256,3))
 imgb = np.random.normal(size=(4,256,256,3))
 
-
-
+ssim1 = K.eval(tf.image.ssim(K.variable(imga), K.variable(imgb), max_val=1.0))
+ssim2 = K.eval(tf.image.ssim(K.variable(imga), K.variable(imga), max_val=1.0))
 resulting_loss1 = K.eval(dssim_loss(K.variable(imga),K.variable(imgb)))
 resulting_loss2 = K.eval(dssim_loss(K.variable(imga),K.variable(imga)))
 
