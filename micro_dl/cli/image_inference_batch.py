09f68ced5863b59cf0eade47d4bcc10dbd3d2967
Syuan-Ming Guo <symguo@gmail.com>
2019-10-18 15:34:36 -0700

    update with new models

--- micro_dl/cli/image_inference_batch.py
+++ micro_dl/cli/image_inference_batch.py
@@ -17,9 +17,9 @@ def sub_process(argin):
     print(p.stdout)
 
 if __name__ == '__main__':
-    model_path = '/CompMicro/Projects/virtualstaining/kidneyslice/' \
-                 '2019_02_15_kidney_slice/models_kidney_20190215'
-    model_dirs = [
+    # model_path = '/CompMicro/Projects/virtualstaining/kidneyslice/' \
+                 # '2019_02_15_kidney_slice/models_kidney_20190215'
+    # model_dirs = [
         # 'Stack_fltr16_256_do20_otus_MAE_1chan_ret_nuclei_pix_iqr_norm',
         # 'Stack_fltr16_256_do20_otus_MAE_1chan_ret_actin_pix_iqr_norm',
         # 'Stack_fltr16_256_do20_otus_MAE_1chan_bf_actin_pix_iqr_norm',
@@ -30,25 +30,27 @@ if __name__ == '__main__':
         # 'Stack_fltr16_256_do20_otus_MAE_4chan_bf_nuclei_pix_iqr_norm',
         # 'Stack_fltr16_256_do20_otus_MAE_4chan_phase_actin_pix_iqr_norm',
         # 'Stack_fltr16_256_do20_otus_MAE_4chan_bf_actin_pix_iqr_norm',
-        '2D_fltr16_256_do20_otus_MAE_1chan_ret_actin_pix_iqr_norm_v3',
+        # '2D_fltr16_256_do20_otus_MAE_1chan_ret_actin_pix_iqr_norm_v3',
         # 'Stack_fltr16_256_do20_otus_MAE_3chan_ret+ori_actin_pix_iqr_norm',
         # 'Stack_fltr16_256_do20_otus_MAE_3chan_ret+ori_nuclei_pix_iqr_norm',
         # 'Stack3_fltr16_256_do20_otus_MAE_1chan_ret_actin_pix_iqr_norm_v3',
         # 'Stack_fltr16_256_do20_otus_MAE_4chan_phase_nuclei_pix_iqr_norm_v3',
         # 'Stack7_fltr16_256_do20_otus_MAE_1chan_ret_actin_pix_iqr_norm_v3',
 
-    ]
-    image_path = '/CompMicro/Projects/virtualstaining/kidneyslice/2019_02_15_kidney_slice/SMS_2018_1227_1433_1_SMS_2018_1227_1433_1_registered'
+    # ]
+    # image_path = '/CompMicro/Projects/virtualstaining/kidneyslice/2019_02_15_kidney_slice/SMS_2018_1227_1433_1_SMS_2018_1227_1433_1_registered'
 
-    # model_path = '/CompMicro/Projects/brainarchitecture/train_pool_H9_H78_GW20_GW24/models'
-    #
-    # model_dirs = ['pool_H9_H78_GW20_GW24_2D_MAE_phase_myelin_regis_pix_iqr_norm',
-    #               'pool_H9_H78_GW20_GW24_Stack_MAE_3chan_ret+ori_myelin_regis_pix_iqr_norm',
-    #               'pool_H9_H78_GW20_GW24_Stack_MAE_4chan_bf_myelin_regis_pix_iqr_norm',
-    #               'pool_H9_H78_GW20_GW24_2D_MAE_4chan_bf_myelin_regis_pix_iqr_norm',
-    #               'pool_H9_H78_GW20_GW24_2D_MAE_4chan_phase_myelin_regis_pix_iqr_norm',
-    #               ]
+    model_path = '/CompMicro/Projects/brainarchitecture/train_pool_H9_H78_GW20_GW24/models'
+
+    model_dirs = ['pool_H9_H78_GW20_GW24_2D_MAE_phase_myelin_regis_stack_norm',
+                  # 'pool_H9_H78_GW20_GW24_2D_MAE_phase_myelin_regis_pix_iqr_norm',
+                  # 'pool_H9_H78_GW20_GW24_Stack_MAE_3chan_ret+ori_myelin_regis_pix_iqr_norm',
+                  # 'pool_H9_H78_GW20_GW24_Stack_MAE_4chan_bf_myelin_regis_pix_iqr_norm',
+                  # 'pool_H9_H78_GW20_GW24_2D_MAE_4chan_bf_myelin_regis_pix_iqr_norm',
+                  # 'pool_H9_H78_GW20_GW24_2D_MAE_4chan_phase_myelin_regis_pix_iqr_norm',
+                  ]
     # image_path = '/CompMicro/Projects/brainarchitecture/train_pool_H9_H78_GW20_GW24'
+    image_path = '/CompMicro/Projects/brainarchitecture/2019_06_21_GW20point5_H30-135_594Fluoromyelin_10X_overlapping/SMS_20190621_1550_3_SMS_20190621_1550_3_fit_order2_registered'
     argin_list = []
     n_workers = 3
     gpu_ids = itertools.cycle(range(1, 4))
@@ -64,6 +66,18 @@ if __name__ == '__main__':
                          ' --metrics ',
                          'coeff_determination ssim pearson_corr'])
 
+        argin = ''.join(['python cli/image_inference.py --model_dir ',
+                         os.path.join(model_path, model_dir),
+                         # ' --save_to_model_dir ',
+                         # '--test_data ',
+                         ' --save_to_image_dir ',
+                         '--all_data ',
+                         '--image_dir ', image_path,
+                         ' --save_figs ',
+                         '--ext .tif ',
+                         '--gpu ', str(gpu_id),
+                         ' --metrics ',
+                         'coeff_determination ssim pearson_corr'])
         argin = shlex.split(argin)
         argin_list.append(argin)
 
