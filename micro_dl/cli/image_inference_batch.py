be680fbaddd048f4369f26a413cc732223cb45e6
Syuan-Ming Guo <symguo@gmail.com>
2019-10-29 15:59:25 -0700

    update with new models

--- micro_dl/cli/image_inference_batch.py
+++ micro_dl/cli/image_inference_batch.py
@@ -36,15 +36,7 @@ if __name__ == '__main__':
         # 'Stack3_fltr16_256_do20_otus_MAE_1chan_ret_actin_pix_iqr_norm_v3',
         # 'Stack_fltr16_256_do20_otus_MAE_4chan_phase_nuclei_pix_iqr_norm_v3',
         # 'Stack7_fltr16_256_do20_otus_MAE_1chan_ret_actin_pix_iqr_norm_v3',
-        # '2D_fltr16_256_do0_otus_MAE_1chan_ret_actin_pix_iqr_norm_bnn_low_lr',
-        # '2D_fltr16_256_do0_otus_MSE_1chan_ret_actin_pix_iqr_norm_bnn_low_lr',
-        # '2D_fltr16_256_do0_otus_MSE_1chan_ret_actin_pix_iqr_norm_bnn',
-        # '2D_fltr16_256_do0_otus_MSE_1chan_ret_actin_bnn_var',
-        # '2D_fltr16_256_do0_otus_MSE_1chan_ret_actin_bnn_100X_var',
-        # '2D_fltr16_256_do0_otus_MSE_1chan_ret_actin_bnn_10X_var',
-        # '2D_fltr16_256_do0_otus_MSE_1chan_ret_actin_bnn_1000X_var',
-        # '2D_fltr16_256_do0_otus_MSE_1chan_ret_actin_bnn_no_var',
-        '2D_fltr16_256_do0_otus_MSE_1chan_ret_actin_bnn_10X_var_10X_reg',
+        '2D_fltr16_256_do0_otus_MSE_1chan_ret_actin_bnn_log_var',
     ]
     image_path = '/CompMicro/Projects/virtualstaining/kidneyslice/2019_02_15_kidney_slice/SMS_2018_1227_1433_1_SMS_2018_1227_1433_1_registered'
 
